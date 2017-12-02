74f1f12b8c40ad2d45af5b78d4acbff8d7265e2f
Jay <jayroberts91@gmail.com>
2017-12-02 15:44:20 -0800

    Saves urls to csv

--- CommunityImages_cld.py
+++ CommunityImages_cld.py
@@ -9,7 +9,8 @@
 - If Top100Games.csv does not exist Top100Games.py is called to create it.
 - If they don't exist 'Gameurls' and 'Gameurls/game_name' directories are created 
 for each game. 
-- In 'Gameurls/game_name' a text file containing the URLs of images from the games steam community is created/updated.
+- In 'Gameurls/game_name' a csv file containing the URLs of images from the games steam community is created/updated.
+the csv has a DOWNLOADED column. 0 = not downloaded, 1 = downloaded, or error-message
  """
 
 from lxml import html
@@ -31,22 +32,29 @@ def main(Num_scrolls = 5, save = True):
         Game_df = pd.read_csv('Top100Games_cld.csv')
     if not os.path.isdir('Gameurls'):
         os.makedirs('Gameurls')
-    
-    
-    # Make a dictionary to hold all the game_url dataframes
-    # keys will be appID as a string
-    Game_urls = {}
 
     for index,row in Game_df.iterrows():
         appID, name = int(row['STEAM ID']), row['GAME']
         game = str(appID)
 
-        # Make the an entry in the dicitionary for the game urls
-        Game_urls[str(appID)] = []
+        # Make a directory for each game to hold a text file of the urls
+        game_url_path = 'Gameurls/'+game
+
+        #Check if the game has a directory
+        if not os.path.isdir(game_url_path):
+            os.makedirs(game_url_path)
+
+        # See if the csv has been made
+        if os.path.exists(game_url_path+'/'+game+'_urls.csv'):
+            game_url_df = pd.read_csv(game_url_path+'/'+game+'_urls.csv')
+        else:
+            game_url_df = {'URL':[],'DOWNLOADED':[]}
+            game_url_df = pd.DataFrame(game_url_df)
 
         print('Game: ', name)
+        # Scrape through with each scroll
+        game_urls = []
         for scroll in range(Num_scrolls):
-            
             # Make the game community url
             game_com = 'http://steamcommunity.com/app/'+ game +'/homecontent/?screenshotspage='+str(scroll)+'&numperpage=100&browsefilter=mostrecent&browsefilter=mostrecent&l=english&appHubSubSection=2&filterLanguage=default&searchText=&forceanon=1'
             
@@ -61,29 +69,28 @@ def main(Num_scrolls = 5, save = True):
                 game_tree = html.fromstring(game_pg.content)
 
                 #Scrape for the sources of the images displayed
-                game_img_urls = game_tree.xpath('//img[@class = "apphub_CardContentPreviewImage"]/@src')
+                new_urls = game_tree.xpath('//img[@class = "apphub_CardContentPreviewImage"]/@src')
 
             # IF the community does not exist
             except:
                 print(name + ' community not found')
                 game_img_urls = ['Community Not Found']
 
-            Game_urls[str(appID)] += game_img_urls
+            game_urls += new_urls
         
-        print('Saving urls')
-        # Make a directory for each game to hold a text file of the urls
-        # ignore repeated urls
-        game_urls = list(set(Game_urls[game]))
+        # put the new urls together and get rid of repeats
+        game_urls = list(set(game_urls))
+        found_urls_df =pd.DataFrame( {'URL': game_urls, 'DOWNLOADED': [0]*len(game_urls)})
 
-        #The saving path
-        game_url_path = 'Gameurls/'+game
-        #Check if the game has a directory
-        if not os.path.isdir(game_url_path):
-            os.makedirs(game_url_path)
+        # append the new data        
+        game_url_df = game_url_df.append(found_urls_df)
+
+        #Drop Duplicates
+        game_url_df.drop_duplicates(subset = 'URL')
+
+        print('Saving URLs')
+        game_url_df.to_csv(game_url_path+'/'+game+'_urls.csv',index = False)
+
         
-        #Save as txt file that's easy to open            
-        with open(game_url_path+'/'+game+'urls.txt', 'w') as f:
-            for url in game_urls:
-                f.write(url + '\n')
 if __name__ == '__main__':
     main()
