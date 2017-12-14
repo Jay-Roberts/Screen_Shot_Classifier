fc8e2672683429910a3f843cafc52a44a7b74eae
Jay <jayroberts91@gmail.com>
2017-12-13 22:15:44 -0800

    Parallelized

--- Downloader_cld.py
+++ Downloader_cld.py
@@ -10,6 +10,7 @@
 import os
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
 # request is for python3 not 2
 #from urllib import request
 from urllib2 import urlopen
@@ -18,12 +19,78 @@ from skimage import transform, io
 import Top100Games_cld
 import CommunityImages_cld
 
-def main(resolution = (224,224,3), all = True, begin = 0, end = 100, save = True):
-    """
-    INPUTS:{resolution: the resolution of the processed image, first: index of first image to be processed (inclusive)
-    last: index of last image to be processed (not inclusive), save: if true saves the images locally}
-    RETURNS: List of processed images from index first to last. Images stored in numpy arrays of size resolution.
-    """
+
+# Take a url and return an image
+def get_images(url,ID,tag):
+    try:
+        # request is for python3
+        #img = request.urlopen(url)
+        if tag % 5 ==0:
+            print(ID+': requesting image '+str(tag))
+        img = urlopen(url)
+        img = misc.imread(img, mode='RGB')
+
+    except:
+        print('Image not found')
+    
+    return img
+
+# Save an image with unique tag
+def img_saver(img_ID_tag):
+    img,ID,tag = img_ID_tag
+    img_path = 'GameImages/'+ID+'/'+ID+'_'+str(tag)+'.jpg'
+    if tag % 5 ==0:
+        print(ID+': saving image '+str(tag))
+    io.imsave(img_path,img)
+
+# Take a list of the form [(url,ID,ix)] and download the images to disk
+def image_collector(url_ID_tag):
+    b_size = len(url_ID_tag)
+    images = [0]*b_size
+    for i_im in range(b_size):
+        url,ID,tag = url_ID_tag[i_im]
+        img = get_images(url,ID,tag)
+        images[i_im] = (img,ID,tag)
+    
+    map(img_saver,images)
+
+# Break up a games urls into chunks
+def url_chunks(ID):
+    # Make an image directory if not already
+    if not os.path.isdir('GameImages/'+ID):
+        os.makedirs('GameImages/'+ID)
+
+    # Get the csv as DF
+    path = 'Gameurls/'+ID+'/'
+    if not os.path.exists(path):
+        print(ID+': Delete Gamesurl and run again')
+
+    url_df = pd.read_csv(path+ID+'_urls.csv')
+
+    # Only get urls that haven't been downloaded before
+    url_list = url_df.loc[url_df['DOWNLOADED'] == 0.0,'URL']
+    labels = [ID]*len(url_list)
+    tag  = list(range(len(url_list)))
+
+    # zip up urls with their ID and tag
+    url_list = zip(url_list,labels,tag)
+
+    
+    size = 10
+
+    chunks = len(url_list)/size
+    orphans = len(url_list)%size
+
+    url_blocks = [0]*chunks
+    for i in range(chunks):
+        url_blocks[i] = url_list[size*i:size*(i+1)]
+
+    url_blocks.append(url_list[-orphans:])
+    
+    return url_blocks
+    
+if __name__ == '__main__':
+    
     # Get the top100games list
     t100_df = pd.read_csv('Top100Games_cld.csv')
 
@@ -36,86 +103,17 @@ def main(resolution = (224,224,3), all = True, begin = 0, end = 100, save = True
         CommunityImages_cld.main()
         
     game_dir = os.listdir('Gameurls')
+    
+    for game in game_dir:
+        print('Starting '+game)
+        url_blocks = url_chunks(game)
+        # Find how many resources are available
+        num_slaves = mp.cpu_count()
+        pool = mp.Pool(processes = num_slaves)
 
-    # Loop through each directory
-    for ID in game_dir:
-        print(ID)
+        pool.map(image_collector,url_blocks)  
+        print('Finished '+game)
 
-        # Make an image directory if not already
-        if not os.path.isdir('GameImages/'+ID):
-            os.makedirs('GameImages/'+ID)
-
-        # Get the csv as DF
-        path = 'Gameurls/'+ID+'/'
-        if not os.path.isfile(path):
-            print('Delete Gamesurl and then run again')
-        
-        url_df = pd.read_csv(path+ID+'_urls.csv')
-        # Only get urls that haven't been downloaded before
-        url_list = url_df.loc[url_df['DOWNLOADED'] == 0.0,'URL']
-        
-        # Hold image files to be saved later
-        if all:
-            first = 0
-            last = len(url_list)
-        else:
-            first = begin
-            last = end
-        
-        images = [0]*(last -first)
-        print('Downloading: %d images'%(last-first))
-        for ix in range(first,last):
-            url = url_list[ix]
-
-            #Net issues could stop this
-            try:
-                # request is for python3
-                #img = request.urlopen(url)
-                print('Requesting img %d/%d'%(ix,num_imgs))
-                img = urlopen(url)
-                img = misc.imread(img, mode='RGB')
-
-                # Process the image
-                img = transform.resize(img,resolution)
-
-                # Put it in the working image list
-                # also store its index for unique identifier
-                images[ix-first] = [img,ix]
-
-            except:
-                print('Image not found')
-
-
-        if save:
-            # Save the images this will change in real cloud version but for testing it saves locally
-            print('Saving Images')
-            num_imgs = len(images)
-            for imgx  in range(num_imgs):
-
-                # Only grab the images that went through
-                if not images[imgx] ==0:
-
-                    # Save the image
-                    img, tag = images[imgx]
-                    img_path = 'GameImages/'+ID+'/'+ID+'_'+str(tag)+'.jpg'
-                    io.imsave(img_path,img)
-
-                    # Update the download column
-                    url_df.iloc[tag]['DOWNLOADED'] = 1
-
-                    if ix % 50 == 0:
-                        print('Saving image %d/%d'%(tag,num_imgs))
-            
-            # Update the Top100games     
-            print('Updating csvs')
-            t100_df.loc[t100_df['STEAM ID'] == int(ID), 'IMAGES'] += num_imgs
-            t100_df.to_csv('Top100Games_cld.csv')
-            url_df.to_csv(path+ID+'_urls.csv')
-        else:
-            return images
-
-if __name__ == '__main__':
-    main()
 
 
     
