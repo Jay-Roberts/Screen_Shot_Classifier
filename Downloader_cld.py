b74a76a8c082a10f4f0e826bec04e614a66a7b2c
Jay <jayroberts91@gmail.com>
2017-12-15 09:32:38 -0800

    Download in chunks

--- Downloader_cld.py
+++ Downloader_cld.py
@@ -5,8 +5,6 @@
 #
 #---------------------------------------------------------------------------
 
-
-
 import os
 import numpy as np
 import pandas as pd
@@ -19,76 +17,48 @@ from skimage import transform, io
 import Top100Games_cld
 import CommunityImages_cld
 
-
 # Take a url and return an image
 def get_images(url,ID,tag):
     try:
         # request is for python3
         #img = request.urlopen(url)
-        if tag % 5 ==0:
+        if tag % 50 ==0:
             print(ID+': requesting image '+str(tag))
         img = urlopen(url)
         img = misc.imread(img, mode='RGB')
 
+        return [img,0]
     except:
         print('Image not found')
-    
-    return img
+        return [0,1]
 
 # Save an image with unique tag
 def img_saver(img_ID_tag):
     img,ID,tag = img_ID_tag
     img_path = 'GameImages/'+ID+'/'+ID+'_'+str(tag)+'.jpg'
-    if tag % 5 ==0:
+    if tag % 50 ==0:
         print(ID+': saving image '+str(tag))
     io.imsave(img_path,img)
 
-# Take a list of the form [(url,ID,ix)] and download the images to disk
-def image_collector(url_ID_tag):
-    b_size = len(url_ID_tag)
-    images = [0]*b_size
-    for i_im in range(b_size):
-        url,ID,tag = url_ID_tag[i_im]
-        img = get_images(url,ID,tag)
-        images[i_im] = (img,ID,tag)
-    
-    map(img_saver,images)
+# Take (url,ID,ix,d) and download the image to disk
+def image_collector(url_ID_tag_d):
+    url,ID,tag,d = url_ID_tag_d
 
-# Break up a games urls into chunks
-def url_chunks(ID):
-    # Make an image directory if not already
-    if not os.path.isdir('GameImages/'+ID):
-        os.makedirs('GameImages/'+ID)
+    if d == 0.0:
+        img, stat = get_images(url,ID,tag)
+        if stat == 0:
+            img_ID_tag= (img,ID,tag)
+            img_saver(img_ID_tag)
+            return 1.0
+        else:
+            return 2.0
+    else:
+        return 1.0
 
-    # Get the csv as DF
-    path = 'Gameurls/'+ID+'/'
-    if not os.path.exists(path):
-        print(ID+': Delete Gamesurl and run again')
+def block_image_collector(urls_IDs_tags_ds):
+    result = map(image_collector,urls_IDs_tags_ds)
+    return list(result)
 
-    url_df = pd.read_csv(path+ID+'_urls.csv')
-
-    # Only get urls that haven't been downloaded before
-    url_list = url_df.loc[url_df['DOWNLOADED'] == 0.0,'URL']
-    labels = [ID]*len(url_list)
-    tag  = list(range(len(url_list)))
-
-    # zip up urls with their ID and tag
-    url_list = zip(url_list,labels,tag)
-
-    
-    size = 100
-
-    chunks = len(url_list)/size
-    orphans = len(url_list)%size
-
-    url_blocks = [0]*chunks
-    for i in range(chunks):
-        url_blocks[i] = url_list[size*i:size*(i+1)]
-
-    url_blocks.append(url_list[-orphans:])
-    
-    return url_blocks
-    
 if __name__ == '__main__':
     
     # Get the top100games list
@@ -106,14 +76,49 @@ if __name__ == '__main__':
     
     for game in game_dir:
         print('Starting '+game)
-        url_blocks = url_chunks(game)
+        ID = game
+
+        # Make an image directory if not already
+        if not os.path.isdir('GameImages/'+ID):
+            os.makedirs('GameImages/'+ID)
+
+        # Get the csv as DF
+        path = 'Gameurls/'+ID+'/'
+        if not os.path.exists(path):
+            print(ID+': Delete Gamesurl and run again')
+
+        url_df = pd.read_csv(path+ID+'_urls.csv')
+
+        # Get the relevant info
+        url_list = url_df['URL']
+        downs = url_df['DOWNLOADED']
+        IDs = [game]*len(url_list)
+        tags = list(url_df.index)
+
+        # Zip it up
+        url_data = zip(url_list,IDs,tags,downs)
+
+        size = 100
+        chunks = len(url_data)/size
+        orphans = len(url_data)%size
+
+        url_blocks = [0]*chunks
+        for i in range(chunks):
+            url_blocks[i] = url_data[i*size:(i+1)*size]
+        
+        if orphans != 0:
+            url_blocks.append(url_data[-orphans:])
+        
         # Find how many resources are available
-        num_slaves = mp.cpu_count()-1
+        num_slaves = mp.cpu_count() - 1
         pool = mp.Pool(processes = num_slaves)
+        url_chunks = pool.map(block_image_collector,url_blocks)  
 
-        pool.map(image_collector,url_blocks)  
+        new_url_list = []
+        for x in url_chunks:
+            new_url_list+= x
+        
+        # update urllist
+        url_df['DOWNLOADED'] = new_url_list
+        url_df.to_csv(path+ID+'_urls.csv')
         print('Finished '+game)
-
-
-
-    
