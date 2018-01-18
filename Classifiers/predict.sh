f8c052ad19e5609422880c948ac7badabf94b335
Jay <jayroberts91@gmail.com>
2018-01-18 15:36:26 -0800

    Add predict.sh

---
+++ Classifiers/predict.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script picks the most recently trained NN 
+# and loads it for inference/predictions on 
+# new data sets
+
+CHKPT_FILE=$(find . -type f -name model.ckpt* -exec stat --format '%Y :%y %n' "{}" \; \
+                | sort -nr \
+                | cut -d: -f2- \
+                | head -n 1 \
+                | cut -d ' ' --fields 4 \
+                | cut -d . --fields 2,3)
+#echo $CHKPT_FILE
+
+python ./estimator/trainer/loadit.py -d $CHKPT_FILE
